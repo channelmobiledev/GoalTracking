8c4d493284d6db4bf67f88f6a4fcf92c61d7059a
Channel Mobiledev <channel.mobiledev@gmail.com>
2021-04-28 18:47:16 +0100

    #8 - Add category list

--- src/components/GoalCategory/goalcategory.screen.tsx
+++ src/components/GoalCategory/goalcategory.screen.tsx
@@ -1,14 +1,65 @@
 import React from 'react';
-import {Text} from 'react-native-paper';
+import {FlatList} from 'react-native';
+import {List, Text} from 'react-native-paper';
 
 /**
  * Goal Category Screen
  */
 const GoalCategoryScreen = () => {
   /**
+   * Category data
+   */
+  const data = [
+    {
+      id: 1,
+      title: 'Financial',
+      icon: 'currency-usd',
+    },
+    {
+      id: 2,
+      title: 'Heath',
+      icon: 'dumbbell',
+    },
+    {
+      id: 3,
+      title: 'Career',
+      icon: 'briefcase-outline',
+    },
+    {
+      id: 4,
+      title: 'Relationship',
+      icon: 'heart-pulse',
+    },
+    {
+      id: 5,
+      title: 'Education',
+      icon: 'book-open-page-variant',
+    },
+  ];
+
+  /**
+   * Render Item List
+   */
+  const renderItem = ({item}: any) => (
+    <List.Item
+      title={item.title}
+      onPress={() => {}}
+      left={props => <List.Icon {...props} icon={item.icon} />}
+    />
+  );
+
+  /**
    * Render
    */
-  return <Text>Hello :D</Text>;
+  return (
+    <>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </>
+  );
 };
 
 export default GoalCategoryScreen;
